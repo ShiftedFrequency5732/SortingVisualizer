c9312e1a42bc4186c7f9c93243631cafa8d529ca
ShiftedFrequency5732 <48628117+ShiftedFrequency5732@users.noreply.github.com>
2023-09-25 19:09:48 +0200

    Created initial window

---
+++ include/config.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+constexpr int INITIAL_WIDTH = 800;
+constexpr int INITIAL_HEIGHT = 800;
+constexpr const char* WINDOW_TITLE = "Sorting Visualizer";
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
+#include "../include/config.hpp"
+
 #include "../include/raylib.h"
 #include "../include/raymath.h"
 
-#include <iostream>
-
 int main() {
-    std::cout << "Hello World" << std::endl;
+    // Initialize the window with the width, the height, the title, set the target FPS, make it resizable.
+    InitWindow(INITIAL_WIDTH, INITIAL_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        // Clear the window from previous loop.
+        ClearBackground(BLANK);
+
+        EndDrawing();
+    }
+
     return 0;
 }
 
